b3b7aaac1dab91099093d42428b738ef12b283ec
alfnsnff <alfnsnff@gmail.com>
2024-04-03 23:16:42 +0700

    Minor Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,16 +49,16 @@ android {
 }
 
 dependencies {
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.activity:activity-ktx:1.4.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -67,5 +67,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    ksp("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.5.2")
 }
